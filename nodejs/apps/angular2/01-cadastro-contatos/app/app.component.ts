7b46de90d4749878d2ea5327c01432a646d58602
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-12-20 20:19:17 -0200

    Added Bootstrap 4.

--- nodejs/apps/angular2/01-cadastro-contatos/app/app.component.ts
+++ nodejs/apps/angular2/01-cadastro-contatos/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>My First Angular 2 App</h1>
+        
+        <button class="btn btn-success">Hello Bootstrap</button>`
+        
 })
 export class AppComponent {
     
