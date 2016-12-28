e66ceb14ee8e9ee9182be4c95fed442f670afe9e
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-12-28 19:13:18 -0200

    Asynchronous calls with Promises

--- nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato.service.ts
+++ nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Contato } from './contato.model';
 import { CONTATOS } from './contatos-mock';
 
 @Injectable()
 export class ContatoService {
-    
-     getContatos(): Contato[] {
-             return CONTATOS;
-     }
+
+    getContatos(): Promise<Contato[]> {
+        return Promise.resolve( CONTATOS );
+    }
 }
