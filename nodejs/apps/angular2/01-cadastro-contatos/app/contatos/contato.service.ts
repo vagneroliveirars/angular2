48d2cd5a7314d0fe5ea6b88f701b3b1fc4168b8e
vagneroliveirars <vagner.oliveirars@gmail.com>
2017-01-04 19:08:19 -0200

    Simulating a slow connection with Promises

--- nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato.service.ts
+++ nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato.service.ts
@@ -9,4 +9,10 @@ export class ContatoService {
     getContatos(): Promise<Contato[]> {
         return Promise.resolve( CONTATOS );
     }
+
+    getContatosSlowly(): Promise<Contato[]> {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, 6000);
+        }).then(() => this.getContatos());
+    }
 }
