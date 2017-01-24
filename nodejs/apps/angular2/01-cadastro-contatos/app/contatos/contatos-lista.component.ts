3ecf807f42d8a03ffb44275618e636e58f56477e
vagneroliveirars <vagner.oliveirars@gmail.com>
2017-01-24 14:18:11 -0200

    Added alert messages.

--- nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contatos-lista.component.ts
+++ nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contatos-lista.component.ts
@@ -10,21 +10,35 @@ import { DialogService } from './../dialog.service';
   templateUrl: 'contatos-lista.component.html'
 })
 export class ContatosListaComponent implements OnInit {
+
     contatos: Contato[];
+    mensagem: {};
+    classesCss: {};
 
     constructor(
         private contatoService: ContatoService,
         private dialogService: DialogService) {}
     
+    /**
+     * Carrega a lista de contatos
+     */
     ngOnInit(): void {
+        // Chama o serviço que retorna a lista de contatos
         this.contatoService.getContatos()
             .then((contatos: Contato[]) => {
                 this.contatos = contatos;
             }).catch(err => {
-                console.log('Aconteceu um erro: ', err);
+                console.log(err);
+                this.mostrarMensagem({
+                    tipo: 'danger',
+                    texto: 'Ocorreu um erro ao buscar a lista de contatos!'
+                });
             });
     }    
 
+    /**
+     * Deleta um contato
+     */
     onDelete(contato: Contato): void {
         // Chama o serviço para confirmar a remoção do contato
         this.dialogService.confirm('Deseja deletar o contato ' + contato.nome + '?')
@@ -36,10 +50,52 @@ export class ContatosListaComponent implements OnInit {
                         .then(() => {
                             // Remove o contato da lista de contatos
                             this.contatos = this.contatos.filter((c : Contato) => c.id != contato.id);
+
+                            this.mostrarMensagem({
+                                tipo: 'success',
+                                texto: 'Contato deletado!'
+                            });                            
                         }).catch(error => {
                             console.log(error);
+                            this.mostrarMensagem({
+                                tipo: 'danger',
+                                texto: 'Ocorreu um erro ao deletar o contato!'
+                            });
                         });
                 }
             });
     }
+
+    /**
+     * Mostra a mensagem e esconde depois de 3 segundos
+     */
+    private mostrarMensagem(mensagem: {tipo: string, texto: string}): void {
+        this.mensagem = mensagem;
+        this.montarClasses(mensagem.tipo);
+        // Mensagem desaparece depois de 3 segundos
+        setTimeout(() => {
+            this.mensagem = undefined;
+        }, 3000);
+    }
+
+    /**
+     * Monta a classe dinamicamente conforme o tipo passado como argumento
+     */
+    private montarClasses(tipo: string): void {
+        /*
+            Exemplo de mensagem de sucesso
+            {
+                'alert': true;
+                'alert-success': true,
+                'alert-danger': false,
+                ...
+            }
+        */
+        this.classesCss = {
+            'alert': true
+        };
+   
+        this.classesCss['alert-' + tipo] = true; // alert-success
+    }
+
 } 
