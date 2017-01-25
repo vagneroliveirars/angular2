e1bc406376e8e888936aefebbe9e35345b2054b4
vagneroliveirars <vagner.oliveirars@gmail.com>
2017-01-25 14:01:10 -0200

    Added  method to navigates back in the platform's history.

--- nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
+++ nodejs/apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
@@ -65,7 +65,11 @@ export class ContatoDetalheComponent implements OnInit {
             promise = this.contatoService.update(this.contato);
         }
 
-        promise.then(contato => this.location.back());
+        promise.then(contato => this.goBack());
+    }
+
+    goBack(): void {
+        this.location.back();
     }
 
 }
